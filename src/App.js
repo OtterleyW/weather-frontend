e92b7654d75ccfcaa648d336ba9681ae6c7625ee
Jenni Nurmi <jenniknurmi@gmail.com>
2018-01-28 19:15:19 +0200

    Show cities from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const Cities = ({cities}) => {
+  const rows = cities.map(city => <span key={city.id}>{city.name} ({city.location})<br /></span>)
+  
+  return (
+    <p>
+      {rows}
+    </p>
+  )
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cities: []
+    }
+  }
+
+  componentWillMount() {
+    axios
+      .get('http://localhost:3001/api/cities')
+      .then(
+        response => {
+        this.setState({ cities: response.data.data })
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Eeppinen säähavainto app</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <Cities cities={this.state.cities} />
+        </div>
       </div>
     );
   }
